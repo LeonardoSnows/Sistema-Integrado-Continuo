1fb527b2db6b3b1c341eb1fa773f163fa83b2307
Icaro Marques <icarokhorus@hotmail.com>
2023-04-21 21:42:13 -0300

    iniciado tela de loading

---
+++ sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/paginaCarregamento/pagina_carregamento.kt
@@ -0,0 +1,12 @@
+package br.com.fatec.projeto.sistemaintegradocontinuo.paginaCarregamento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fatec.projeto.sistemaintegradocontinuo.R
+
+class pagina_carregamento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina_carregamento)
+    }
+}
