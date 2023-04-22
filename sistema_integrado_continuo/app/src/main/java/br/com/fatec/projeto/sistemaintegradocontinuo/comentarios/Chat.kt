583fee308d32aebd01d710edfd025ccdd25c5305
Icaro Marques <icarokhorus@hotmail.com>
2023-04-22 15:58:13 -0300

    correção de tela

--- sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/comentarios/Chat.kt
+++ sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/comentarios/Chat.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.fatec.projeto.sistemaintegradocontinuo.R
 
-class chat : AppCompatActivity() {
+class Chat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
