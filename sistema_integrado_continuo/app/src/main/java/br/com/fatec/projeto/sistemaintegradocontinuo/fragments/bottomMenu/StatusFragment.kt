5c83dcb2b76d674ac2c06c36674ee37e5a687970
lucasdvsantos <santoslucasdavid@gmail.com>
2023-06-11 14:40:16 -0300

    Tela de status mostrando as OS' e os status, filtrando por nome e status e popup com botão para redirecionar para a tela de comentários e editar

--- sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/fragments/bottomMenu/StatusFragment.kt
+++ sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/fragments/bottomMenu/StatusFragment.kt
@@ -2,33 +2,46 @@ package br.com.fatec.projeto.sistemaintegradocontinuo.fragments.bottomMenu
 
 
 import OSAdapter
+import android.app.Dialog
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.PopupWindow
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.compose.ui.graphics.Color
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.fatec.projeto.sistemaintegradocontinuo.R
-import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class StatusFragment : Fragment() {
+class StatusFragment : Fragment(){
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var osAdapter: OSAdapter
-
+    private lateinit var sSearch: Spinner
     private lateinit var etSearch: EditText
+    private lateinit var btnSearch: ImageButton
+    private lateinit var btnFilter: ImageButton
+    private var osTitulo : MutableList<String> = mutableListOf()
+    private var filteredList: MutableList<OsItem> = mutableListOf()
 
-    private lateinit var allOS: List<OSItem>
+    private var isSearchingByName = true
+
+//    private lateinit var allOS: List<OSItem>
 
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
@@ -42,8 +55,7 @@ class StatusFragment : Fragment() {
 //        recyclerView.adapter = adapter
 //    }
 
-
-    data class OSItem(val title: String)
+    data class OsItem(val id: String, val details: Map<String, Any>)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,74 +63,140 @@ class StatusFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_status, container, false)
-
+        sSearch = view.findViewById(R.id.sSearch)
         etSearch = view.findViewById(R.id.etSearch)
+        btnSearch = view.findViewById(R.id.btnSearch)
+        btnFilter = view.findViewById(R.id.btnFilter)
 
-        var osItems: List<StatusFragment.OSItem> = listOf()
+        var osItems : List<OsItem> = listOf()
 
         lifecycleScope.launch {
             osItems = pegarOS("1")
 
+//            val selectedKey = "titulo"
+//            var desiredItem : Any? = ""
+//            osTitulo = mutableListOf()
+//
+//            for ((outerKey, innerMap) in osItems) {
+//                for ((innerKey, value) in innerMap) {
+//                    if (innerKey == selectedKey) {
+//                        desiredItem = innerMap[innerKey]
+//                        osTitulo.add(desiredItem as String)
+//                    }
+//                }
+//            }
+
+            filteredList.addAll(osItems)
+
             recyclerView = view.findViewById(R.id.rvOrdens)
-            recyclerView.layoutManager = LinearLayoutManager(activity)
-            osAdapter = OSAdapter(osItems)
+            osAdapter = OSAdapter(filteredList)
             recyclerView.adapter = osAdapter
+            recyclerView.layoutManager = LinearLayoutManager(activity)
+
+
+            btnFilter.setOnClickListener {
+                toggleSearchOption()
+            }
+
+            btnSearch.setOnClickListener {
+                val query = if (isSearchingByName) {
+                    etSearch.text.toString().trim()
+                } else {
+                    sSearch.selectedItem.toString()
+                }
+                osAdapter.performSearch(query,isSearchingByName)
+            }
         }
 
-        etSearch.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // Not needed
-            }
+        val statusOptions = listOf("Pendente", "Em Andamento", "Aguardando Aprovação", "Finalizada", "Cancelada")
+        val spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, statusOptions)
+        sSearch.adapter = spinnerAdapter
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // Filter the data based on the search query
-                val query = s.toString().trim()
-                val filteredItens = filterItens(osItems, query)
-
-                // Update the RecyclerView adapter with the filtered data
-                osAdapter = OSAdapter(osItems)
-                recyclerView.adapter = osAdapter
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                // Not needed
-            }
-        })
+//        etSearch.addTextChangedListener(object : TextWatcher {
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//                // Not needed
+//            }
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                // Filter the data based on the search query
+//                val query = s.toString().trim()
+//                val filteredItens = filterItens(osItems, query)
+//
+//                // Update the RecyclerView adapter with the filtered data
+//                osAdapter = OSAdapter(osItems)
+//                recyclerView.adapter = osAdapter
+//            }
+//
+//            override fun afterTextChanged(s: Editable?) {
+//                // Not needed
+//            }
+//        })
 
 
         return view
     }
 
 
-    private fun filterItens(itens: List<OSItem>, query: String): List<OSItem> {
-        val filteredList = mutableListOf<OSItem>()
-        for (iten in itens) {
-            if (iten.title.contains(query, true)) {
-                filteredList.add(iten)
-            }
+    private fun toggleSearchOption() {
+        isSearchingByName = !isSearchingByName
+
+        // Toggle visibility of UI elements based on the selected search option
+        if (isSearchingByName) {
+            etSearch.visibility = View.VISIBLE
+            sSearch.visibility = View.GONE
+        } else {
+            etSearch.visibility = View.GONE
+            sSearch.visibility = View.VISIBLE
         }
-        return filteredList
     }
 
-    private suspend fun pegarOS(idEmpresa: String): List<OSItem> {
+//    private fun performSearch() {
+//        val query = if (isSearchingByName) {
+//            etSearch.text.toString().trim()
+//        } else {
+//            sSearch.selectedItem.toString()
+//        }
+//
+////        val query = etSearch.text.toString().trim()
+//
+//        filteredList.clear()
+//        if (query.isEmpty()) {
+//            filteredList.addAll(osTitulo)
+//        } else {
+//            filteredList.addAll(osTitulo.filter { it.contains(query, true) })
+//        }
+//
+//        osAdapter.notifyDataSetChanged()
+//    }
+
+
+//    private fun filterItens(itens: List<OSItem>, query: String): List<OSItem> {
+//        val filteredList = mutableListOf<OSItem>()
+//        for (iten in itens) {
+//            if (iten.title.contains(query, true)) {
+//                filteredList.add(iten)
+//            }
+//        }
+//        return filteredList
+//    }
+
+    private suspend fun pegarOS(idEmpresa: String) : List<OsItem> {
         // Pegando a instancia do Firestore e as OSs referente a empresa que foi passada
         val db: FirebaseFirestore = FirebaseFirestore.getInstance()
-        val osList = mutableListOf<OSItem>()
-        val listaOS =
-            db.collection("Ordem_servico").whereEqualTo("empresa", idEmpresa).get().await()
+//        val osList = mutableListOf<Any>()
+        var osDados = mutableListOf<OsItem>()
+        val listaOS = db.collection("Ordem_servico").whereEqualTo("empresa", idEmpresa).get().await()
+        var number = 0
+        val string = "OS"
+        var id = ""
 
-
-        for (os in listaOS) {
-            val osDados = os.data
-            val osTitulo = osDados["titulo"] as? String
-            println(osDados)
-            osTitulo?.let {
-                osList.add(OSItem(it))
-            }
+        listaOS.forEach { os ->
+            val osMap: MutableMap<String, Any> = os.data
+            id = string + number
+            val item = OsItem(id,osMap)
+            osDados.add(item)
+            number++
         }
-
-        return osList
+        return osDados
     }
-
-
 }
--- sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/fragments/bottomMenu/adapter/OSAdapter.kt
+++ sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/fragments/bottomMenu/adapter/OSAdapter.kt
@@ -1,10 +1,19 @@
+import android.app.Dialog
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.fatec.projeto.sistemaintegradocontinuo.R
 import br.com.fatec.projeto.sistemaintegradocontinuo.fragments.bottomMenu.StatusFragment
+import kotlinx.coroutines.CoroutineScope
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.Objects
 
 //class OSAdapter(private val osList: List<String>) : RecyclerView.Adapter<OSAdapter.OSViewHolder>() {
 //
@@ -30,27 +39,86 @@ import br.com.fatec.projeto.sistemaintegradocontinuo.fragments.bottomMenu.Status
 
 
 
-class OSAdapter(private val osItems: List<StatusFragment.OSItem>) : RecyclerView.Adapter<OSAdapter.OSViewHolder>() {
+class OSAdapter(private val osItems: List<StatusFragment.OsItem>) : RecyclerView.Adapter<OSAdapter.OSViewHolder>() {
 
+    private var filteredList: List<StatusFragment.OsItem> = osItems
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OSViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_os, parent, false)
         return OSViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: OSViewHolder, position: Int) {
-        val osItem = osItems[position]
-        holder.bind(osItem)
+        val osItem = filteredList[position]
+        holder.osTextView.text = osItem.details["titulo"] as CharSequence?
+        holder.osStatusView.text = osItem.details["status"] as CharSequence?
+
+        holder.itemView.setOnClickListener {
+            val dialog = Dialog(holder.itemView.context)
+            dialog.setContentView(R.layout.popup_layout)
+
+            val osTitle = dialog.findViewById<TextView>(R.id.osTitle)
+            val osStatus = dialog.findViewById<TextView>(R.id.osStatus)
+            val osDesc = dialog.findViewById<TextView>(R.id.osDesc)
+            val osDate = dialog.findViewById<TextView>(R.id.osDate)
+
+            osTitle.text = osItem.details["titulo"] as CharSequence?
+            osStatus.text = osItem.details["status"] as CharSequence?
+            osDesc.text = osItem.details["descricao"] as CharSequence?
+            val timestamp = osItem.details["data_solicitacao"] as com.google.firebase.Timestamp
+            val date = timestamp.toDate()
+            val readableDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(date)
+
+            osDate.text = readableDate
+
+            // Customize the dialog layout and its views
+            // For example:
+            val closeButton = dialog.findViewById<ImageView>(R.id.closeButton)
+            closeButton.setOnClickListener {
+                dialog.dismiss() // Dismiss the dialog when the close button is clicked
+            }
+
+            // Show the dialog
+            dialog.show()
+        }
     }
 
     override fun getItemCount(): Int {
-        return osItems.size
+        return filteredList.size
     }
 
     inner class OSViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bind(osItem: StatusFragment.OSItem) {
-            // Bind the data to your view holder's views
-            itemView.findViewById<TextView>(R.id.tvOs).text = osItem.title
-        }
+        val osTextView: TextView = itemView.findViewById(R.id.tvOs)
+        val osStatusView : TextView = itemView.findViewById(R.id.tvOsStatus)
+
+//        init {
+//            osTextView.setOnClickListener{
+//                val position = bindingAdapterPosition
+//                onItemClick(position)
+//            }
+//        }
     }
+
+    fun performSearch(query: String, searchByName: Boolean) {
+        filteredList = if (query.isNotEmpty()) {
+            osItems.filter { item ->
+                if (searchByName) {
+                    item.details["titulo"].toString().contains(query, ignoreCase = true) ?: false
+                } else {
+                    item.details["status"].toString().contains(query, ignoreCase = true) ?: false
+                }
+//                when (searchBy) {
+//                    "ID" -> item.id.contains(query, ignoreCase = true)
+//                    "Name" -> item.details["name"]?.contains(query, ignoreCase = true) ?: false
+//                    "Status" ->
+//                    else -> false
+//                }
+            }
+        } else {
+            osItems
+        }
+        notifyDataSetChanged()
+    }
+
+
 }
