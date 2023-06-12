682ef5ac85ca33cb8da143577d84fa097a8f6bfc
icaromarquess <icarokhorus@hotmail.com>
2023-06-12 15:36:37 -0300

    criada a tela de listar cliente com botão de ver ordens

--- sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/fragments/bottomMenu/ListarClientesFragment.kt
+++ sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/fragments/bottomMenu/ListarClientesFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeEmpresaFragment.newInstance] factory method to
+ * Use the [ListarClientesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeEmpresaFragment : Fragment() {
+class ListarClientesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -32,7 +32,7 @@ class HomeEmpresaFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_empresa, container, false)
+        return inflater.inflate(R.layout.fragment_listar_clientes, container, false)
     }
 
     companion object {
@@ -42,11 +42,11 @@ class HomeEmpresaFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeEmpresaFragment.
+         * @return A new instance of fragment ListarClientesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic fun newInstance(param1: String, param2: String) =
-                HomeEmpresaFragment().apply {
+                ListarClientesFragment().apply {
                     arguments = Bundle().apply {
                         putString(ARG_PARAM1, param1)
                         putString(ARG_PARAM2, param2)
---
+++ sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/fragments/bottomMenu/adapter/ClienteAdapter.kt
@@ -0,0 +1,4 @@
+package br.com.fatec.projeto.sistemaintegradocontinuo.fragments.bottomMenu.adapter
+
+class ClienteAdapter {
+}
