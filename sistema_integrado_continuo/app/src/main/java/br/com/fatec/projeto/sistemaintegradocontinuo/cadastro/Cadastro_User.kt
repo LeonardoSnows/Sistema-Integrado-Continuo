7b2c619de7ba917664794ee11f07ed6984d20244
Welling7onSiqueira <guiton.guimaraes@gmail.com>
2023-06-11 22:02:30 -0300

    Atualização Cadastro

--- sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/cadastro/Cadastro_User.kt
+++ sistema_integrado_continuo/app/src/main/java/br/com/fatec/projeto/sistemaintegradocontinuo/cadastro/Cadastro_User.kt
@@ -11,10 +11,15 @@ import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import br.com.fatec.projeto.sistemaintegradocontinuo.R
 import br.com.fatec.projeto.sistemaintegradocontinuo.formlogin.FormLogin
+import br.com.fatec.projeto.sistemaintegradocontinuo.funcoes_uteis.requestViaCep
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.w3c.dom.Text
 
 class Cadastro_User : AppCompatActivity() {
@@ -45,33 +50,110 @@ class Cadastro_User : AppCompatActivity() {
         val senha =
             findViewById<TextInputEditText>(R.id.senha_cadastro_material_text).text.toString()
                 .trim();
+        val nome =
+            findViewById<TextInputEditText>(R.id.nome_empresa_text).text.toString()
+                .trim();
+        val cnpj =
+            findViewById<TextInputEditText>(R.id.cnpj_text).text.toString()
+                .trim();
+        val fone =
+            findViewById<TextInputEditText>(R.id.telefone_text).text.toString()
+                .trim();
+        val cep =
+            findViewById<TextInputEditText>(R.id.cep_text).text.toString()
+                .trim();
+        val numero =
+            findViewById<TextInputEditText>(R.id.numero_text).text.toString()
+                .trim();
 
         if (email.isEmpty() || senha.isEmpty()) {
-            val snackbar =
-                Snackbar.make(view, "Preencha todos os campos!", Snackbar.LENGTH_SHORT)
+            val snackbar = Snackbar.make(view, "Preencha todos os campos", Snackbar.LENGTH_SHORT)
             snackbar.setBackgroundTint(Color.RED)
             snackbar.show()
         } else {
             auth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener { task ->
-                 if (task.isSuccessful) {
+                val value = if (task.isSuccessful) {
+                    criarEmpresa(nome, email, cnpj, fone, cep, numero)
                     Toast.makeText(
                         this,
-                        "Email $email foi registrado com Sucesso !!!",
+                        "Email ${email} foi registrado com sucesso",
                         Toast.LENGTH_LONG
-                    )
-                        .show();
-                    val user = auth.currentUser
+                    ).show()
                 } else {
                     Toast.makeText(
                         this, " Email invalido !!! !!!",
                         Toast.LENGTH_LONG
                     ).show();
-                     val exception = task.exception;
-                     println("Erro ao cadastrar o usuário: ${exception?.message}")
-                 }
+                    val exception = task.exception;
+                    println("Erro ao cadastrar o usuário: ${exception?.message}")
+                }
             }
         }
+    }
+}
 
+private fun criarEmpresa(
+    nome_empresa: String,
+    email: String,
+    cnpj: String,
+    telefone: String,
+    cep: String,
+    numero: String
+) {
+    // pegando os dados do endereço pelo ViaCep
+    runBlocking {
+        val viaCepResponse = async(Dispatchers.IO) {
+            requestViaCep(cep)
+        }
 
+        val endereco = viaCepResponse.await()
+        if (endereco != null) {
+            val rua = endereco.logradouro
+            val bairro = endereco.bairro
+            val cidade = endereco.localidade
+            val estado = endereco.uf
+
+            if (email != null) {
+                // Criar uma referência para o documento da empresa usando o email como ID
+                val empresasRef = FirebaseFirestore.getInstance().collection("Empresa")
+                val empresaDoc = empresasRef.document(email)
+
+                // Verificar se o documento da empresa já existe
+                empresaDoc.get()
+                    .addOnSuccessListener { documentSnapshot ->
+                        if (documentSnapshot.exists()) {
+                            // Retornar que o Email já está cadastrado
+
+                        } else {
+                            // O documento da empresa não existe, crie um novo documento
+                            val novaEmpresa = hashMapOf(
+                                "nome_empresa" to nome_empresa,
+                                "cnpj" to cnpj,
+                                "telefone" to telefone,
+                                "cep" to cep,
+                                "numero" to numero,
+                                "rua" to rua,
+                                "bairro" to bairro,
+                                "cidade" to cidade,
+                                "estado" to estado,
+                                "email" to email,
+                            )
+                            empresaDoc.set(novaEmpresa)
+                                .addOnSuccessListener {
+                                    // A criação foi bem-sucedida
+                                }
+                                .addOnFailureListener { exception ->
+                                    println(exception.message)
+                                }
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        // Tratar erros durante a verificação do documento
+                    }
+            }
+
+        } else {
+            println("Não foi possível obter as informações do CEP.")
+        }
     }
 }
